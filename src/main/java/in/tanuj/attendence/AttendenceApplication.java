68c2ec1e8ed0d8626651b0e2ec08b10c60ba0e23
TANUJ-args <tanujmargana@gmail.com>
2026-03-05 23:52:40 +0530

    Added dependency

---
+++ src/main/java/in/tanuj/attendence/AttendenceApplication.java
@@ -0,0 +1,13 @@
+package in.tanuj.attendence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/tanuj/attendence/AttendenceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tanuj.attendence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
