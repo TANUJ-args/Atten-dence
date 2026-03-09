a4d6c2a57ca9168d097f4f00d95472a449c41412
TANUJ-args <tanujmargana@gmail.com>
2026-03-09 20:55:31 +0530

    Adding new user call login reg api

--- src/main/java/in/tanuj/attendence/entity/UserEntity.java
+++ src/main/java/in/tanuj/attendence/entity/UserEntity.java
@@ -1,5 +1,4 @@
 package in.tanuj.attendence.entity;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,11 +8,14 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name="std_db")
-public class StudentEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int rollNo;
-    private String name;
-    private String email;
+@Table(name="user_db")
+public class UserEntity {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int id;
+
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private String email;
+   
+   private String password;
 }
---
+++ src/main/java/in/tanuj/attendence/service/UserService.java
@@ -0,0 +1,5 @@
+package in.tanuj.attendence.service;
+
+public class UserService {
+    
+}
